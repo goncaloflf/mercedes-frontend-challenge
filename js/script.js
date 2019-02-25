4c2e6d3b42ff020c9666ea3af9ee6aeb0e1347d1
goncaloflf <goncaloflf@hotmail.com>
2019-02-25 19:52:11 +0000

    stuff

--- js/script.js
+++ js/script.js
@@ -7,3 +7,53 @@ document.body.addEventListener("click", function(event) {
 		document.getElementById("header").classList.remove("open");
 	}
 });
+
+document
+	.getElementById("form-modal")
+	.addEventListener("click", function(event) {
+		if (!event.target.closest(".edit-box")) {
+			closeModal();
+		}
+	});
+
+document
+	.getElementById("add-new-content")
+	.addEventListener("click", function() {
+		openModal();
+	});
+
+document.getElementById("cancel-btn").addEventListener("click", function() {
+	closeModal();
+});
+
+document
+	.getElementById("checkbox-image")
+	.addEventListener("change", function(e) {
+		if (e.target.checked) {
+			document.querySelector("input[name='image']").disabled = false;
+			document.getElementById("checkbox-cta").checked = false;
+			let event = new Event("change");
+			document.getElementById("checkbox-cta").dispatchEvent(event);
+		} else {
+			document.querySelector("input[name='image']").disabled = true;
+		}
+	});
+document.getElementById("checkbox-cta").addEventListener("change", function(e) {
+	if (e.target.checked) {
+		document.querySelector("input[name='cta']").disabled = false;
+		document.getElementById("checkbox-image").checked = false;
+
+		let event = new Event("change");
+		document.getElementById("checkbox-image").dispatchEvent(event);
+	} else {
+		document.querySelector("input[name='cta']").disabled = true;
+	}
+});
+
+function closeModal() {
+	document.body.classList.remove("modal-open");
+}
+
+function openModal() {
+	document.body.classList.add("modal-open");
+}
